a83fba238152aa1694ffc3731046807bb5befd4d
Myrmex <dfusi@hotmail.com>
2021-03-28 12:03:08 +0200

    Updated packages

--- Pack.bat
+++ Pack.bat
@@ -2,6 +2,10 @@
 echo BUILD Cadmus Pura Packages
 del .\Cadmus.Pura.Parts\bin\Debug\*.nupkg
 del .\Cadmus.Pura.Parts\bin\Debug\*.snupkg
+del .\Cadmus.Pura.Services\bin\Debug\*.nupkg
+del .\Cadmus.Pura.Services\bin\Debug\*.snupkg
+del .\Cadmus.Seed.Pura.Parts\bin\Debug\*.nupkg
+del .\Cadmus.Seed.Pura.Parts\bin\Debug\*.snupkg
 
 cd .\Cadmus.Pura.Parts
 dotnet pack -c Debug -p:IncludeSymbols=true -p:SymbolPackageFormat=snupkg
